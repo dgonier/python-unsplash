fea1521f468bce9f78b9d2d30416fe6b2c17c45e
Devin Gonier <dgonier@gmail.com>
2023-08-24 13:44:55 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,10 +13,10 @@ setup(
     url="http://github.com/yakupadakli/python-unsplash.git",
     packages=find_packages(exclude=["tests"]),
     install_requires=[
-        "oauthlib==2.0.1",
+        "oauthlib>=2.0.1",
         "requests==2.27.1",
-        "requests-oauthlib==0.7.0",
-        "six==1.10.0",
+        "requests-oauthlib>=0.7.0",
+        "six>=1.10.0",
     ],
     keywords="unsplash library",
     classifiers=[
